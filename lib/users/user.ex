38ab48db9450d40fe4a208287f476cb7bfdee011
walterbarreiro <barreirowalter@live.com.ar>
2023-12-03 17:51:46 -0300

    Integro implementacion registry

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -17,31 +17,31 @@ defmodule User do
 
   # --- Callbacks ---
 
-  def handle_call({:get_messages, chat_pid}, _from, {user_state}) do
-    #{:ok, chat_pid} = find_chat(user_state.user_name, message.receiver.user_name)
+  def handle_call({:get_messages, receiver}, _from, {user_state}) do
+    {:ok, chat_pid} = find_chat(user_state.id, receiver)
     {:reply, Chat.get_messages(chat_pid), {user_state}}
   end
 
-  def handle_cast({:add_message, message = %Message{}, chat_pid}, {user_state}) do
-    #{:ok, chat_pid} = find_chat(user_state.user_name, message.receiver.user_name)
+  def handle_cast({:add_message, message = %Message{}}, {user_state}) do
+    {:ok, chat_pid} = find_chat(user_state.id, message.receiver.id)
     Chat.add_message(chat_pid, message)
     {:noreply, {user_state}}
   end
 
-  def handle_cast({:modify_message, message_id, new_text, receiver = %UserState{}, chat_pid}, {user_state}) do
-    #{:ok, chat_pid} = find_chat(user_state.user_id, receiver.user_id)
+  def handle_cast({:modify_message, message_id, new_text, receiver}, {user_state}) do
+    {:ok, chat_pid} = find_chat(user_state.id, receiver)
     Chat.modify_message(chat_pid, message_id, new_text)
     {:noreply, {user_state}}
   end
 
-  def handle_cast({:delete_message, message_id, receiver = %UserState{}, chat_pid}, {user_state}) do
-    #{:ok, chat_pid} = find_chat(user_state.user_id, receiver.user_id)
+  def handle_cast({:delete_message, message_id, receiver}, {user_state}) do
+    {:ok, chat_pid} = find_chat(user_state.id, receiver)
     Chat.delete_message(chat_pid, message_id)
     {:noreply, {user_state}}
   end
 
-  def handle_cast({:delete_messages, message_ids, receiver = %UserState{}, chat_pid}, {user_state}) do
-    #{:ok, chat_pid} = find_chat(user_state.user_id, receiver.user_id)
+  def handle_cast({:delete_messages, message_ids, receiver}, {user_state}) do
+    {:ok, chat_pid} = find_chat(user_state.id, receiver)
     Chat.delete_messages(chat_pid, message_ids)
     {:noreply, {user_state}}
   end
@@ -52,24 +52,24 @@ defmodule User do
 
   # --- Funciones ---
 
-  def read_messages(user_pid, chat_pid) do
-    GenServer.call(user_pid, {:get_messages, chat_pid})
+  def read_messages(user_pid, receiver) do
+    GenServer.call(user_pid, {:get_messages, receiver})
   end
 
-  def send_message(user_pid, message, chat_pid) do
-    GenServer.cast(user_pid, {:add_message, message, chat_pid})
+  def send_message(user_pid, message) do
+    GenServer.cast(user_pid, {:add_message, message})
   end
 
-  def modify_message(user_pid, message_id, new_text, receiver, chat_pid) do
-    GenServer.cast(user_pid, {:modify_message, message_id, new_text, receiver, chat_pid})
+  def modify_message(user_pid, message_id, new_text, receiver) do
+    GenServer.cast(user_pid, {:modify_message, message_id, new_text, receiver})
   end
 
-  def delete_message(user_pid, message_id, receiver, chat_pid) do
-    GenServer.cast(user_pid, {:delete_message, message_id, receiver, chat_pid})
+  def delete_message(user_pid, message_id, receiver) do
+    GenServer.cast(user_pid, {:delete_message, message_id, receiver})
   end
 
-  def delete_messages(user_pid, message_ids, receiver, chat_pid) do
-    GenServer.cast(user_pid, {:delete_messages, message_ids, receiver, chat_pid})
+  def delete_messages(user_pid, message_ids, receiver) do
+    GenServer.cast(user_pid, {:delete_messages, message_ids, receiver})
   end
 
   def read_notifications(user_pid) do
@@ -77,6 +77,7 @@ defmodule User do
   end
 
   def find_chat(sender_id, receiver_id) do
-    Chats.Registry.find_or_create_chat(sender_id, receiver_id)
+    users = [sender_id, receiver_id]
+    Chats.Registry.find_or_create(users)
   end
 end
