599b367b82aff232210e2043e19eb90bd5375989
digerzo <prediger.agustin@gmail.com>
2023-12-05 00:36:08 -0300

    agrego Dockerfile y scripts de starteo de nodos

--- lib/application.ex
+++ lib/application.ex
@@ -2,15 +2,21 @@ defmodule Pigeon.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
 
     children = [
-      Chats.ChatSupervisor
-      #%{id: Chats.ChatSupervisor, start: {Chats.ChatSupervisor, :start_link, [[]]} },
+      {Cluster.Supervisor, [topologies(), [name: Pigeon.ClusterSupervisor]]},
+      # Chats.ChatSupervisor
     ]
 
-    opts = [strategy: :one_for_one, name: Pigeon.Supervisor, max_seconds: 5, max_restarts: 3]
+    opts = [strategy: :one_for_one, max_seconds: 5, max_restarts: 3]
     Supervisor.start_link(children, opts)
   end
 
+  defp topologies do
+    [
+      key: [
+        strategy: Cluster.Strategy.Gossip
+      ]
+    ]
+  end
 end
---
+++ node_2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+iex --sname node2 --cookie some_cookie -S mix
